bbfba8a7f1117f7e166bbf45e1990cf7930761cc
WdataW <wael272726@gmail.com>
2025-07-01 22:34:01 +0300

    feat: added user pfp to the nav bar and made it responive

--- src/components/ui/navigations/MainNav.jsx
+++ src/components/ui/navigations/MainNav.jsx
@@ -2,15 +2,16 @@ import { useTheme, surfaceBgColors } from "../../../context/Theme";
 import Nav from "./Nav";
 import { StatsNav, ProfileNav, HomeNav, TasksNav } from "./navItems";
 
-export default function MainNav({className="", ulClassName="", children,...props}){
+export default function MainNav({className="", invert=false ,ulClassName="", children,...props}){
     const [theme] = useTheme();
     const bgColor =surfaceBgColors[theme];
     return(
-        <Nav ulClassName={ulClassName} className={`${className} ${bgColor}`} {...props}>
-            <HomeNav active={true} />
+        <Nav  ulClassName={ulClassName} className={`${className} ${bgColor}`} {...props}>
+            {invert && <ProfileNav />}
+            <HomeNav active={true}/>
             <TasksNav/>
             <StatsNav/>
-            <ProfileNav/>
+            {!invert && <ProfileNav />}
             {children}
         </Nav>
     );
--- src/components/ui/navigations/navItems/ProfileNav.jsx
+++ src/components/ui/navigations/navItems/ProfileNav.jsx
@@ -1,16 +1,9 @@
 import { useTranslation } from "../../../../context/Language";
 import { useTheme } from "../../../../context/Theme";
 import NavItem from "./NavItem";
-const icons = {
-    dark:"bg-[url(/src/assets/icons/dark/profile.svg)]",
-    light:"bg-[url(/src/assets/icons/light/profile.svg)]"
-}
+
 export default function ProfileNav({className="" , children, ...props}){
-    const [theme] = useTheme();
     const t = useTranslation();
     return(
-        <NavItem className={`${className}`} image={icons[theme]} {...props}>
-            {t("titles.profile")}
-        </NavItem>
-    );
+        <NavItem className={`${className} justify-center`} image={`bg-[url(/images/userPFP.png)] bg-cover rounded-full h-[2rem] w-[2rem] outline-[0.075rem] outline-offset-2 `} {...props}/>    );
 }
