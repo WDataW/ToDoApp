332baebe379b9cafb7fbfff418d15dd04ed3a209
WdataW <wael272726@gmail.com>
2025-07-30 20:19:13 +0300

    Added a message to show when the tags search returns an empty array

--- src/components/ui/Tasks/CategoriesSection.jsx
+++ src/components/ui/Tasks/CategoriesSection.jsx
@@ -28,8 +28,8 @@ export default function CategoriesSection({ activeTags, setActiveTags, className
     return (
         <section className={className}>
             <SearchInput required={false} value={tagsFilter} handleChange={handleSearchChange} placeholder={t("terms.searchTag")} className="mb-[0.9rem] max-w-[20rem]"></SearchInput>
+            {filteredTags.length == 0 && <p>{t("terms.noMatchingTags")}</p>}
             <ul className="tags-list gap-3 flex items-center justify-start pb-[0.3rem] overflow-x-auto h-[7.5rem]">
-
                 {filteredTags.map((tag, i) =>
                     <li key={i}>
                         <TaskCategory i={i} active={activeTags} handleClick={handleTagClick} tag={tag} className="min-w-[10rem]" />
